40be3a5ddfd86fb43c7d863c66573f9c304d5b2d
erbdev <erbdev@hotmail.com>
2024-10-22 13:38:46 +0100

    Refactor StatisticPage component to fetch customer bill items and display statistics

--- client/src/pages/StatisticPage.jsx
+++ client/src/pages/StatisticPage.jsx
@@ -1,19 +1,33 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Header from "../components/Header/Header";
 import StatisticCard from "../components/statistics/StatisticCard";
 import { Area, Pie } from "@ant-design/plots";
 
 const StatisticPage = () => {
   const [data, setData] = useState([]);
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
     asyncFetch();
   }, []);
 
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const response = await fetch(
+          "http://localhost:5000/api/products/get-all"
+        );
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getProducts();
+  }, []);
+
   const asyncFetch = () => {
-    fetch(
-      "https://gw.alipayobjects.com/os/bmw-prod/360c3eae-0c73-46f0-a982-4746a6095010.json"
-    )
+    fetch("http://localhost:5000/api/bills/get-all")
       .then((response) => response.json())
       .then((json) => setData(json))
       .catch((error) => {
@@ -21,37 +35,10 @@ const StatisticPage = () => {
       });
   };
 
-  const data2 = [
-    {
-      type: "分类一",
-      value: 27,
-    },
-    {
-      type: "分类二",
-      value: 25,
-    },
-    {
-      type: "分类三",
-      value: 18,
-    },
-    {
-      type: "分类四",
-      value: 15,
-    },
-    {
-      type: "分类五",
-      value: 10,
-    },
-    {
-      type: "其他",
-      value: 5,
-    },
-  ];
-
   const config = {
     data,
-    xField: "timePeriod",
-    yField: "value",
+    xField: "customerName",
+    yField: "subTotal",
     xAxis: {
       range: [0, 1],
     },
@@ -59,9 +46,9 @@ const StatisticPage = () => {
 
   const config2 = {
     appendPadding: 10,
-    data: data2,
-    angleField: "value",
-    colorField: "type",
+    data,
+    angleField: "subTotal",
+    colorField: "customerName",
     radius: 1,
     innerRadius: 0.6,
     label: {
@@ -89,9 +76,14 @@ const StatisticPage = () => {
           overflow: "hidden",
           textOverflow: "ellipsis",
         },
-        content: "AntV\nG2Plot",
+        content: "Total\nSales",
       },
     },
+  };
+
+  const totalAmount = () => {
+    const amount = data.reduce((total, item) => item.totalAmount + total, 0);
+    return `${amount.toFixed(2)} £`;
   };
 
   return (
@@ -108,22 +100,22 @@ const StatisticPage = () => {
           <div className="statistic-cards grid xl:grid-cols-4 md:grid-cols-2 my-10 md:gap-10 gap-4">
             <StatisticCard
               title={"Total Customer"}
-              amount={"10"}
+              amount={data?.length}
               img={"images/user.png"}
             />
             <StatisticCard
               title={"Total Income"}
-              amount={"£ 3 250"}
+              amount={totalAmount()}
               img={"images/money.png"}
             />
             <StatisticCard
               title={"Total Sales"}
-              amount={"8"}
+              amount={data?.length}
               img={"images/sale.png"}
             />
             <StatisticCard
               title={"Total Product"}
-              amount={"55"}
+              amount={products?.length}
               img={"images/product.png"}
             />
           </div>
