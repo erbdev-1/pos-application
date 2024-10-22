c57e75d1a1a09c24d5e5a1638c541d50295d14d4
erbdev <erbdev@hotmail.com>
2024-10-22 11:41:52 +0100

    create invoice page

--- client/src/pages/BillPage.jsx
+++ client/src/pages/BillPage.jsx
@@ -1,40 +1,72 @@
-import React, { useState } from "react";
-import { Table, Card, Button } from "antd";
+import { useEffect, useState } from "react";
+import { Table, Button } from "antd";
 import Header from "../components/Header/Header";
-
 import PrintBill from "../components/bills/PrintBill";
 
 const BillPage = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const dataSource = [
-    {
-      key: "1",
-      name: "Mike",
-      age: 32,
-      address: "10 Downing Street",
-    },
-    {
-      key: "2",
-      name: "John",
-      age: 42,
-      address: "10 Downing Street",
-    },
-  ];
+  const [billItems, setBillItems] = useState([]);
+
+  useEffect(() => {
+    const getBills = async () => {
+      try {
+        const res = await fetch("http://localhost:5000/api/bills/get-all");
+        const data = await res.json();
+        setBillItems(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getBills();
+  }, []);
+
   const columns = [
     {
-      title: "Name",
-      dataIndex: "name",
-      key: "name",
+      title: "Client Name",
+      dataIndex: "customerName",
+      key: "customerName",
     },
     {
-      title: "Age",
-      dataIndex: "age",
-      key: "age",
+      title: "Phone Number",
+      dataIndex: "customerPhoneNumber",
+      key: "customerPhoneNumber",
     },
     {
-      title: "Address",
-      dataIndex: "address",
-      key: "address",
+      title: "Creation Date",
+      dataIndex: "createdAt",
+      key: "createdAt",
+      render: (text) => {
+        return <span>{text.substring(0, 16)}</span>;
+      },
+    },
+    {
+      title: "Payment Mode",
+      dataIndex: "paymentMode",
+      key: "paymentMode",
+    },
+    {
+      title: "Total Amount",
+      dataIndex: "totalAmount",
+      key: "totalAmount",
+      render: (text) => {
+        return <span>£ {text}</span>;
+      },
+    },
+    {
+      title: "Actions",
+      dataIndex: "actions",
+      key: "actions",
+      render: (text) => {
+        return (
+          <Button
+            type="link"
+            className="pl-0"
+            onClick={() => setIsModalOpen(true)}
+          >
+            Print
+          </Button>
+        );
+      },
     },
   ];
   return (
@@ -43,23 +75,11 @@ const BillPage = () => {
       <div className="px-6">
         <h1 className="text-4xl font-bold text-center mb-4">Customer Bills</h1>
         <Table
-          dataSource={dataSource}
+          dataSource={billItems}
           columns={columns}
           bordered
           pagination={false}
         />
-        <div className="cart-total flex justify-end mt-4  ">
-          <Card className="w-72">
-            <Button
-              className="mt-4 w-full"
-              type="primary"
-              size="large"
-              onClick={() => setIsModalOpen(true)}
-            >
-              Print
-            </Button>
-          </Card>
-        </div>
       </div>
       <PrintBill isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
     </>
